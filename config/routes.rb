aa7cb729250b0ea721e2b9d09b3921e499ba7ef8
Brett Middleton <brett01101101@gmail.com>
2018-05-30 14:22:01 -0400

    Revert "commented out avatar pics"
    This reverts commit 6c55f5772662174c55c9bbadfcdf6d5c3bdbfd06.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,14 @@ Rails.application.routes.draw do
   devise_scope :user do
     root to: 'home#index'
   end
+<<<<<<< HEAD
   root to: 'landing_page#index'
+=======
+
+  # if current_user?
+  #   root to: 'home#index'
+  # else
+  #   root to: 'landing_page#index'
+  # end
+>>>>>>> parent of 6c55f57... commented out avatar pics
 end
