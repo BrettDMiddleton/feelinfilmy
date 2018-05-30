9cd91dee7a883676b3459ef4272300d3ffe72741
Neal Sutaria <nealsutaria99@gmail.com>
2018-05-30 11:16:57 -0400

    user-seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,64 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+user = User.new(
+  email: Faker::Internet.email,
+  bio: "I love to watch documentaries.",
+  password: "feelinfilmy",
+  username: Faker::HarryPotter.character,
+  last_name: Faker::Name.last_name,
+  first_name: Faker::Name.first_name
+)
+user.save!
+user = User.new(
+  email: Faker::Internet.email,
+  bio: "I love to watch romantic films.",
+  password: "feelinfilmy",
+  username: Faker::HarryPotter.character,
+  last_name: Faker::Name.last_name,
+  first_name: Faker::Name.first_name
+)
+user.save!
+user = User.new(
+  email: Faker::Internet.email,
+  bio: "I love to watch fantasy films such as harry potter.",
+  password: "feelinfilmy",
+  username: Faker::HarryPotter.character,
+  last_name: Faker::Name.last_name,
+  first_name: Faker::Name.first_name
+)
+user.save!
+user = User.new(
+  email: Faker::Internet.email,
+  bio: "I am a big fan of movies that have a lot of action.",
+  password: "feelinfilmy",
+  username: Faker::HarryPotter.character,
+  last_name: Faker::Name.last_name,
+  first_name: Faker::Name.first_name
+)
+user.save!
+
+user = User.new(
+  email: Faker::Internet.email,
+  bio: "I love to watch any movie. i just love movies in general.",
+  password: "feelinfilmy",
+  username: Faker::HarryPotter.character,
+  last_name: Faker::Name.last_name,
+  first_name: Faker::Name.first_name
+)
+user.save!
+
+
+
+
+
+
+
+
+
+
+
+
