6168dd73b4e57980c20b1153db8d7f0d0820dd9c
Brett Middleton <brett01101101@gmail.com>
2018-05-30 16:57:33 -0400

    tag seed created, homepage started

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as:'rails_admin'
   devise_for :users
+  # root to: 'landing_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_scope :user do
     root to: 'homes#index'
   end
-  root to: 'landing_pages#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,58 +8,6 @@
 
 User.destroy_all
 
-user = User.new(
-  email: Faker::Internet.email,
-  bio: "I love to watch documentaries.",
-  password: "feelinfilmy",
-  username: Faker::HarryPotter.character,
-  last_name: Faker::Name.last_name,
-  first_name: Faker::Name.first_name
-)
-user.remote_avatar_url = "https://res.cloudinary.com/chimeraggeddon/image/upload/v1527699133/feelin-filmy/allef-vinicius-152932-unsplash.jpg"
-user.save!
-user = User.new(
-  email: Faker::Internet.email,
-  bio: "I love to watch romantic films.",
-  password: "feelinfilmy",
-  username: Faker::HarryPotter.character,
-  last_name: Faker::Name.last_name,
-  first_name: Faker::Name.first_name
-)
-user.remote_avatar_url = "https://res.cloudinary.com/chimeraggeddon/image/upload/v1527699133/feelin-filmy/cristian-lozan-371397-unsplash.jpg"
-user.save!
-user = User.new(
-  email: Faker::Internet.email,
-  bio: "I love to watch fantasy films such as harry potter.",
-  password: "feelinfilmy",
-  username: Faker::HarryPotter.character,
-  last_name: Faker::Name.last_name,
-  first_name: Faker::Name.first_name
-)
-user.remote_avatar_url = "https://res.cloudinary.com/chimeraggeddon/image/upload/v1527699134/feelin-filmy/eli-defaria-14556-unsplash.jpg"
-user.save!
-user = User.new(
-  email: Faker::Internet.email,
-  bio: "I am a big fan of movies that have a lot of action.",
-  password: "feelinfilmy",
-  username: Faker::HarryPotter.character,
-  last_name: Faker::Name.last_name,
-  first_name: Faker::Name.first_name
-)
-user.remote_avatar_url = "https://res.cloudinary.com/chimeraggeddon/image/upload/v1527699133/feelin-filmy/ethan-hoover-311143-unsplash.jpg"
-user.save!
-
-user = User.new(
-  email: Faker::Internet.email,
-  bio: "I love to watch any movie. i just love movies in general.",
-  password: "feelinfilmy",
-  username: Faker::HarryPotter.character,
-  last_name: Faker::Name.last_name,
-  first_name: Faker::Name.first_name
-)
-user.remote_avatar_url = "https://res.cloudinary.com/chimeraggeddon/image/upload/v1527699134/feelin-filmy/mubariz-mehdizadeh-364026-unsplash.jpg"
-user.save!
-
 DBMOVIES = [
   "Forest Gump",
   "Alien",
@@ -81,7 +29,77 @@ DBMOVIES = [
   "Arrival",
   "Prometheus",
   "Mad Max"
-  ]
+]
+
+TAGS = [
+  "character development",
+  "acting",
+  "dialogue",
+  "cinematography",
+  "special effects",
+  "characters",
+  "music and sound design",
+  "thought-provoking",
+  "awesome villain",
+  "storyline/plot",
+  "explosions",
+  "witty humour"
+]
+
+user = User.new(
+  email: Faker::Internet.email,
+  bio: "I love to watch documentaries.",
+  password: "feelinfilmy",
+  username: Faker::HarryPotter.character,
+  last_name: Faker::Name.last_name,
+  first_name: Faker::Name.first_name
+)
+# user.remote_avatar_url = "https://res.cloudinary.com/chimeraggeddon/image/upload/v1527699133/feelin-filmy/allef-vinicius-152932-unsplash.jpg"
+user.save!
+user = User.new(
+  email: Faker::Internet.email,
+  bio: "I love to watch romantic films.",
+  password: "feelinfilmy",
+  username: Faker::HarryPotter.character,
+  last_name: Faker::Name.last_name,
+  first_name: Faker::Name.first_name
+)
+# user.remote_avatar_url = "https://res.cloudinary.com/chimeraggeddon/image/upload/v1527699133/feelin-filmy/cristian-lozan-371397-unsplash.jpg"
+user.save!
+user = User.new(
+  email: Faker::Internet.email,
+  bio: "I love to watch fantasy films such as harry potter.",
+  password: "feelinfilmy",
+  username: Faker::HarryPotter.character,
+  last_name: Faker::Name.last_name,
+  first_name: Faker::Name.first_name
+)
+# user.remote_avatar_url = "https://res.cloudinary.com/chimeraggeddon/image/upload/v1527699134/feelin-filmy/eli-defaria-14556-unsplash.jpg"
+user.save!
+user = User.new(
+  email: Faker::Internet.email,
+  bio: "I am a big fan of movies that have a lot of action.",
+  password: "feelinfilmy",
+  username: Faker::HarryPotter.character,
+  last_name: Faker::Name.last_name,
+  first_name: Faker::Name.first_name
+)
+# user.remote_avatar_url = "https://res.cloudinary.com/chimeraggeddon/image/upload/v1527699133/feelin-filmy/ethan-hoover-311143-unsplash.jpg"
+user.save!
+
+user = User.new(
+  email: Faker::Internet.email,
+  bio: "I love to watch any movie. i just love movies in general.",
+  password: "feelinfilmy",
+  username: Faker::HarryPotter.character,
+  last_name: Faker::Name.last_name,
+  first_name: Faker::Name.first_name
+)
+# user.remote_avatar_url = "https://res.cloudinary.com/chimeraggeddon/image/upload/v1527699134/feelin-filmy/mubariz-mehdizadeh-364026-unsplash.jpg"
+user.save!
+
+# Tag Seed
+TAGS.each { |tag| newtag = Tag.new( name: tag ) newtag.save! }
 
   USERS = User.all
 
