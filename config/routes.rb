3680c839b9bb2b833c367757d8ee972ea1bb0337
Brett Middleton <brett01101101@gmail.com>
2018-05-31 11:03:23 -0400

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as:'rails_admin'
   devise_for :users
-  root to: 'landing_pages#index'
+  root to: 'movies#index'
   get 'movie_tags/create'
   get 'user_movies/create'
   get 'user_movies/update'
