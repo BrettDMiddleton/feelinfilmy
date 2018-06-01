cc8ef7efa200f9d860256f5a84896be51a75bf70
Neal Sutaria <nealsutaria99@gmail.com>
2018-06-01 15:34:47 -0400

    searching and toggling

--- app/javascript/packs/tags.js
+++ app/javascript/packs/tags.js
@@ -1,17 +1,21 @@
+
+
 form = document.getElementById("form")
 
 
-  tags = document.querySelectorAll(".btn-filmy");
+tags = document.querySelectorAll(".btn-filmy");
 
-  tags.forEach(function(tag){
-    tag.addEventListener("click", function(){
-       if (this.classList.contains('active')){
-         this.classList.remove('active');
-     } else {
-         this.classList.add('active');
-     }
-    });
+tags.forEach(function(tag){
+  tag.addEventListener("click", function() {
+    if (this.classList.contains('active')) {
+      this.classList.remove('active');
+    } else {
+      this.classList.add('active');
+    }
+
+    document.getElementById("filter-moives").click();
   });
+});
 
 
 
