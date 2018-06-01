d361747292f2d8d405d1527186190560917a3cac
Brett Middleton <brett01101101@gmail.com>
2018-06-01 16:01:59 -0400

    js fix for heroku

---
+++ app/javascript/components/tag.js
@@ -0,0 +1,20 @@
+const initTags = () => {
+  form = document.getElementById("form")
+
+
+  tags = document.querySelectorAll(".btn-filmy");
+
+  tags.forEach(function(tag){
+    tag.addEventListener("click", function() {
+      if (this.classList.contains('active')) {
+        this.classList.remove('active');
+      } else {
+        this.classList.add('active');
+      }
+
+      document.getElementById("filter-moives").click();
+    });
+  });
+
+
+}
