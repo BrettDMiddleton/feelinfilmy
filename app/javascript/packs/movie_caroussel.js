7a39d86cd31b6540561737e7112ddb3d089ba3b4
David Poisson <zeraaiur@hotmail.com>
2018-06-04 15:18:47 -0400

    dashboard-movie

--- app/javascript/packs/movie_caroussel.js
+++ app/javascript/packs/movie_caroussel.js
@@ -5,13 +5,29 @@ var output = document.getElementById("demo");
 slider.oninput = function() {
   let x = parseInt(this.value);
 
-  if (x < movie_length - 5){
 
-    for (i = 0; i < movie_length; i++) {
-      document.getElementById(`my-movie${i}`).classList.add("display-none");
-    }
-    for (i = 0; i < 6; i++){
-      document.getElementById(`my-movie${x+i}`).classList.remove("display-none");
-    }
+  for (i = 0; i < movie_length; i++) {
+    document.getElementById(`my-movie${i}`).classList.add("display-none");
+  }
+  for (i = 0; i < 4; i++){
+    document.getElementById(`my-movie${x+i}`).classList.remove("display-none");
   }
 }
+
+
+for (i = 0; i < movie_length; i++) {
+  document.getElementById(`my-movie${i}`).addEventListener("mouseover", function(){
+    makeVisible(this.id)
+  });
+    document.getElementById(`my-movie${i}`).addEventListener("mouseout", function(){
+    makeInvisible(this.id)
+  });
+
+}
+function makeVisible(x){
+  document.getElementById(`${x}hover`).classList.remove("display-none");
+}
+function makeInvisible(x){
+  document.getElementById(`${x}hover`).classList.add("display-none");
+}
+
