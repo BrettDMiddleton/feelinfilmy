68f29e69ca75e9b74ac4c03e71ee5b8cd2017a12
Neal Sutaria <nealsutaria99@gmail.com>
2018-06-05 16:08:12 -0400

    landing new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as:'rails_admin'
   devise_for :users
-  root to: 'movies#index'
+  root to: 'pages#landing'
+  resources :movies, only: [ :index, :show ]
   get 'movie_tags/create'
   get 'user_movies/create'
   get 'user_movies/update'
   get 'movies/index'
   get "/movies/:id", to: "movies#show", as:"movies_show"
-  get 'pages/landing'
   get 'pages/dashboard'
   resources :profiles, only: [ :show ]
   resources :movie_tags, only: [:create]
