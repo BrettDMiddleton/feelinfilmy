0a98f3811dd4e55c7e33f36089e9d33b7972bf00
David Poisson <zeraaiur@hotmail.com>
2018-06-06 12:08:26 -0400

    change to dashboard

--- app/javascript/packs/movie_caroussel.js
+++ app/javascript/packs/movie_caroussel.js
@@ -1,39 +1,39 @@
 // Update the current slider value (each time you drag the slider handle)
 
-const setSlider = (elementName, movieElementName, movie_length) => {
-  const slider = document.getElementById(elementName);
+// const setSlider = (elementName, movieElementName, movie_length) => {
+//   const slider = document.getElementById(elementName);
 
-  console.log('setSlider', arguments);
-  if (slider) {
-    slider.oninput = function() {
-      let x = parseInt(this.value);
+//   console.log('setSlider', arguments);
+//   if (slider) {
+//     slider.oninput = function() {
+//       let x = parseInt(this.value);
 
 
-      for (i = 0; i < movie_length; i++) {
-        document.getElementById(`${movieElementName}${i}`).classList.add("display-none");
-      }
-      for (i = 0; i < 4; i++){
-        document.getElementById(`${movieElementName}${x+i}`).classList.remove("display-none");
-      }
-    }
-  }
+//       for (i = 0; i < movie_length; i++) {
+//         document.getElementById(`${movieElementName}${i}`).classList.add("display-none");
+//       }
+//       for (i = 0; i < 4; i++){
+//         document.getElementById(`${movieElementName}${x+i}`).classList.remove("display-none");
+//       }
+//     }
+//   }
 
-  for (i = 0; i < movie_length; i++) {
-    document.getElementById(`${movieElementName}${i}`).addEventListener("mouseover", function(){
-      makeVisible(this.id)
-    });
-    document.getElementById(`${movieElementName}${i}`).addEventListener("mouseout", function(){
-      makeInvisible(this.id)
-    });
-  }
-}
+//   for (i = 0; i < movie_length; i++) {
+//     document.getElementById(`${movieElementName}${i}`).addEventListener("mouseover", function(){
+//       makeVisible(this.id)
+//     });
+//     document.getElementById(`${movieElementName}${i}`).addEventListener("mouseout", function(){
+//       makeInvisible(this.id)
+//     });
+//   }
+// }
 
-function makeVisible(x){
-  document.getElementById(`${x}hover`).classList.remove("display-none");
-}
-function makeInvisible(x){
-  document.getElementById(`${x}hover`).classList.add("display-none");
-}
+// function makeVisible(x){
+//   document.getElementById(`${x}hover`).classList.remove("display-none");
+// }
+// function makeInvisible(x){
+//   document.getElementById(`${x}hover`).classList.add("display-none");
+// }
 
 const switchTab = (event) => {
   const catalogue = document.getElementById('my-catalogue');
