7effe0fdc5329f301a9502cb3a8de3c304d16278
Ben Calef <bencalef@gmail.com>
2018-06-06 11:59:40 -0400

    updated meta tags, almost done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     @my_movies = Movie.where.not(id: watched_movies.pluck(:movie_id))
   end
 
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
 end
---
+++ config/initializers/default_meta.rb
@@ -0,0 +1,2 @@
+# Initialize default meta tags
+DEFAULT_META = YAML.load_file(Rail.root.join("config/meta.yml"))
