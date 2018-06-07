1590bacacdf5b632fc922794be860209a2d0d4cd
Neal Sutaria <nealsutaria99@gmail.com>
2018-06-07 10:42:43 -0400

    changes to correct it

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -1,13 +1,13 @@
   class UserMoviesController < ApplicationController
   def create_catalog
     movie_id = params[:movie][:movie_id].to_i
-    @user_movie = UserMovie.create(user_id: current_user.id, movie_id: movie_id, watched: true)
+    @user_movie = UserMovie.create(user_id: current_user.id, movie_id: movie_id, watched: false)
     # raise
   end
 
   def create_watch
     movie_id = params[:movie][:movie_id].to_i
-    @user_movie = UserMovie.create(user_id: current_user.id, movie_id: movie_id, watched: false)
+    @user_movie = UserMovie.create(user_id: current_user.id, movie_id: movie_id, watched: true)
     # raise
   end
 
