e95fc8b780e4f4e6dfd294495f8dd23a3be1ee31
Brett Middleton <brett01101101@gmail.com>
2018-06-07 14:19:19 -0400

    overlay added to show

---
+++ app/assets/javascripts/sidebar.js
@@ -0,0 +1,5 @@
+function sidebarClick(_, target){
+  document.getElementById("movie-sidebar").classList.toggle("open-sidebar")
+  document.getElementById("sidebar-wrapper").classList.toggle("open-bg-sidebar")
+  document.getElementById("sidebar-container").classList.toggle("open-bg-sidebar")
+}
